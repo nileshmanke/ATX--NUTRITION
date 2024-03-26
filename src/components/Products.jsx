d69c0650dd382a1d38fb97a8b39a875c292df17a
Nilesh Ravindra Manke <nileshmanke12ab@gmail.com>
2024-03-26 14:06:27 +0530

    fix card

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -60,7 +60,7 @@ const Products = () => {
 
 
 
-      <div>
+      <div className='m-auto'>
       <div className="card" style={{width: '18rem'}}>
   <img  src="https://sp.yimg.com/ib/th?id=OPAC.iEeT6OM1%2bV1OoQ474C474&o=5&pid=21.1&w=&h=" className="card-img-top p-5 " alt="..."/>
   <div className="card-body flex flex-col gap-y-4">
@@ -74,7 +74,7 @@ const Products = () => {
 
 
       
-      <div>
+      <div className='m-auto'>
       <div className="card" style={{width: '18rem'}}>
   <img  src="https://sp.yimg.com/ib/th?id=OPAC.iEeT6OM1%2bV1OoQ474C474&o=5&pid=21.1&w=&h=" className="card-img-top p-5 " alt="..."/>
   <div className="card-body flex flex-col gap-y-4">
@@ -87,22 +87,7 @@ const Products = () => {
 
 
 
-      <div>
-      <div className="card" style={{width: '18rem'}}>
-  <img  src="https://sp.yimg.com/ib/th?id=OPAC.iEeT6OM1%2bV1OoQ474C474&o=5&pid=21.1&w=&h=" className="card-img-top p-5 " alt="..."/>
-  <div className="card-body flex flex-col gap-y-4">
-    <h2 className="card-title text-xl font-bold">INLIFE 100% WHEY PROTEIN ISOLATE POWDER SUPPLEMENT - (CHOCOLATE)</h2>
-    <p className="card-text">Whey Protein is a type of protein which is a by-product of the cheese-making process. </p>
-    <a href="#" className="btn btn-primary">Add To cart</a>
-  </div>
-</div>
-      </div>
-
-
-
-
-      
-      <div>
+      <div className='m-auto'>
       <div className="card" style={{width: '18rem'}}>
   <img  src="https://sp.yimg.com/ib/th?id=OPAC.iEeT6OM1%2bV1OoQ474C474&o=5&pid=21.1&w=&h=" className="card-img-top p-5 " alt="..."/>
   <div className="card-body flex flex-col gap-y-4">
@@ -117,7 +102,22 @@ const Products = () => {
 
 
       
-      <div>
+      <div className='m-auto'>
+      <div className="card" style={{width: '18rem'}}>
+  <img  src="https://sp.yimg.com/ib/th?id=OPAC.iEeT6OM1%2bV1OoQ474C474&o=5&pid=21.1&w=&h=" className="card-img-top p-5 " alt="..."/>
+  <div className="card-body flex flex-col gap-y-4">
+    <h2 className="card-title text-xl font-bold">INLIFE 100% WHEY PROTEIN ISOLATE POWDER SUPPLEMENT - (CHOCOLATE)</h2>
+    <p className="card-text">Whey Protein is a type of protein which is a by-product of the cheese-making process. </p>
+    <a href="#" className="btn btn-primary">Add To cart</a>
+  </div>
+</div>
+      </div>
+
+
+
+
+      
+      <div className='m-auto'>
       <div className="card" style={{width: '18rem'}}>
   <img  src="https://sp.yimg.com/ib/th?id=OPAC.iEeT6OM1%2bV1OoQ474C474&o=5&pid=21.1&w=&h=" className="card-img-top p-5 " alt="..."/>
   <div className="card-body flex flex-col gap-y-4">
